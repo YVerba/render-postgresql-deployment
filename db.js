1e6b8c7ccd2ca6e01aec651de6041f19739ec179
YVerba <y.verbaaa@gmail.com>
2025-05-19 16:13:25 +0300

    change db

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//   user: "yarik_verba",
+//   password: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   database: "postgres"
+// });
+
 const pool = new Pool({
-  user: "yarik_verba",
-  password: "postgres",
-  host: "localhost",
+  user: "render_postgresql_rmgs_user",
+  password: "PNS8S0Is414WAa4MHgkZENpccYEYUZF1",
+  host: "dpg-d0lir31r0fns738e5udg-a",
   port: 5432,
-  database: "postgres"
+  database: "render_postgresql_rmgs"
 });
 
 export default pool;
