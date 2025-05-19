685391846139f9e861ba8b780cfc0d429d362a4a
YVerba <y.verbaaa@gmail.com>
2025-05-19 16:48:19 +0300

    change host in db

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ import { Pool } from "pg";
 const pool = new Pool({
   user: "render_postgresql_rmgs_user",
   password: "PNS8S0Is414WAa4MHgkZENpccYEYUZF1",
-  host: "dpg-d0lir31r0fns738e5udg-a",
+  host: "dpg-d0lir31r0fns738e5udg-a.oregon-postgres.render.com",
   port: 5432,
   database: "render_postgresql_rmgs"
 });
